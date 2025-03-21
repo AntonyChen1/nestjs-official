060df0dda3e0c35bd3f98be874e752b425a70b85
Antony <antony.chen@bp.liferay.com>
2025-03-21 14:38:08 +0800

    nest g module cats

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,7 +3,6 @@ import { CatsService } from './cats.service';
 import { CatsController } from './cat.controller';
 
 @Module({
-  imports: [CatsModule],
   controllers: [CatsController],
   providers: [CatsService],
 })
